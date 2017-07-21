740a061088ee5b344b6e1f019e31d13925baee09
Felipe Hlibco <hlibco@gmail.com>
2017-07-21 00:09:06 -0700

    change chmod on ssh keys

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@
 # -------------------------------------
 # PREVENT WARNINGS
 # -------------------------------------
-create_ansible_hosts () {
+function create_ansible_hosts {
   ANSIBLE=/usr/local/etc/ansible
   HOSTS=$ANSIBLE/hosts
 
@@ -20,76 +20,66 @@ create_ansible_hosts () {
     echo "localhost ansible_connection=local" > $HOSTS && echo "Created $HOSTS"
   fi
 }
-
 create_ansible_hosts
-
-
-# sudo ls -s /tmp/mysql.sock /usr/local/var/mysql/mysql.sock && echo "Symlinked /tmp/mysql.sock to /usr/local/var/mysql/mysql.sock"
 
 
 # Set permissions
 chown $USER ~/.ssh
-chmod 400 ~/.ssh/*
-chmod 600 ~/.ssh/config
 
-ssh-add -K ~/.ssh/id_rsa
-
-ansible-playbook main.yml --flush-cache
 
 # -------------------------------------
 # START
 # -------------------------------------
-# if test "$(uname)" = "Darwin"; then
-#   if test ! $(which gcc); then
-#     echo "Install Xcode..."
-#     xcode-select --install
-#   fi
-#   if test ! "$(which brew)"; then
-#     echo "Install homebrew..."
-#     sudo chown $(whoami) '/usr/local' '/usr/local/Caskroom' "${HOME}/Library/Caches/Homebrew/"
-#     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#   else
-#     brew update
-#     brew upgrade
-#     brew prune
-#     brew cleanup
-#   fi
-#   if test ! "$(which ansible)"; then
-#     echo "Install Ansible..."
-#     brew install ansible
-#   else
-#     brew upgrade ansible
-#   fi
-# elif test "$(uname)" = "Linux"; then
-#   if test -f /etc/lsb-release && test ! "$(which ansible)"; then
-#     echo "Install Ansible..."
-#     sudo apt-get install -y software-properties-common
-#     sudo apt-add-repository -y ppa:ansible/ansible
-#     sudo apt-get update
-#     sudo apt-get install -y ansible
-#   fi
-#   printf '\n%s\n%s\n' '[privilege_escalation]' 'become = True' >> ansible.cfg
-# fi
+if test "$(uname)" = "Darwin"; then
+  if test ! $(which gcc); then
+    echo "Install Xcode..."
+    xcode-select --install
+  fi
+  if test ! "$(which brew)"; then
+    echo "Install homebrew..."
+    sudo chown $(whoami) '/usr/local' '/usr/local/Caskroom' "${HOME}/Library/Caches/Homebrew/"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    brew update
+    brew upgrade
+    brew prune
+    brew cleanup
+  fi
+  if test ! "$(which ansible)"; then
+    echo "Install Ansible..."
+    brew install ansible
+  else
+    brew upgrade ansible
+  fi
+elif test "$(uname)" = "Linux"; then
+  if test -f /etc/lsb-release && test ! "$(which ansible)"; then
+    echo "Install Ansible..."
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install -y ansible
+  fi
+  printf '\n%s\n%s\n' '[privilege_escalation]' 'become = True' >> ansible.cfg
+fi
 
-# if test ! "$(which ansible)"; then
-#   echo "Not supported yet."
-#   exit 1
-# fi
+if test ! "$(which ansible)"; then
+  echo "Not supported yet."
+  exit 1
+fi
 
-# python --version
-# ansible --version
-# ansible-playbook main.yml --flush-cache
-
-
-# test -d ~/dotfiles && cd $_ && ./bootstrap.sh
-
+python --version
+ansible --version
+ansible-playbook main.yml --flush-cache
 
 # reboot after installation is done
-# `sudo fdesetup isactive`
-# if [[ $? != 0 ]]; then
-#   read -p "##### Do you want to reboot? [Yn]" -n 1 -r
-#   if [[ $REPLY =~ ^[Yy]$ ]]; then
-#     echo ''
-#     sudo reboot
-#   fi
-# fi
+function reboot {
+  `sudo fdesetup isactive`
+  if [[ $? != 0 ]]; then
+    read -p "##### Do you want to reboot? [Yn]" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo ''
+      sudo reboot
+    fi
+  fi
+}
+# reboot
